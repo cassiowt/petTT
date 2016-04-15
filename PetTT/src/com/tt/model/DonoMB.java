ce5742937afa5342e2d8e7e224903d42b3f9bc28
André Santos <andre.leonhardt.santos@gmail.com>
2016-04-15 08:03:56 -0300

    Acerto  MB Dono

---
+++ PetTT/src/com/tt/model/DonoMB.java
@@ -0,0 +1,47 @@
+package com.tt.model;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.ArrayDataModel;
+import javax.faces.model.DataModel;
+
+@ManagedBean(name = "donoMB")
+@SessionScoped
+public class DonoMB implements Serializable {
+
+	private static final long serialVersionUID = 7427334381930431970L;
+
+	private Dono dono;
+	private DataModel donos;
+//	private DonoDAO donoDao;
+
+	public DonoMB() {
+		dono = new Dono();
+		donos = new ArrayDataModel(new Dono[]{
+			new Dono("João", "33224422", "rua blblbl"),
+			new Dono("João", "33224422", "rua blblbl"),
+			new Dono("João", "33224422", "rua blblbl"),
+			new Dono("João", "33224422", "rua blblbl")
+			});
+	}
+
+	public Dono getDono() {
+		return dono;
+	}
+
+	public void setDono(Dono dono) {
+		this.dono = dono;
+	}
+
+	
+	public DataModel<Dono> getDonos() {
+		return donos;
+	}
+
+	public void setDonos(DataModel<Dono> donos) {
+		this.donos = donos;
+	}
+
+}
