9b4777fedc39a3db8ad6bdb9cf4aff60648bef7e
André Santos <andre.leonhardt.santos@gmail.com>
2016-04-15 10:09:11 -0300

    DonoDAO

--- PetTT/src/com/tt/dao/DonoDAO.java
+++ PetTT/src/com/tt/dao/DonoDAO.java
@@ -1,5 +1,7 @@
 package com.tt.dao;
 
+import java.util.Collection;
+
 import javax.persistence.EntityManager;
 
 import com.tt.control.JpaUtil;
@@ -9,18 +11,35 @@ public class DonoDAO {
 
 	static EntityManager fabrica = JpaUtil.getFactory();
 
-	public boolean cadastrarDono(Dono dono) {
-		boolean ok = false;
-
+	public boolean cadastrar(Dono dono) {
 		if (dono != null) {
 			fabrica.getTransaction().begin();
 			fabrica.persist(dono);
 			fabrica.flush();
 			fabrica.getTransaction().commit();
 			fabrica.close();
-			ok = true;
+			return true;
 		}
-		return ok;
-
+		
+		return false;
+	}
+		
+	public Collection<Dono> listar() {
+		javax.persistence.Query query = (javax.persistence.Query) fabrica.createNamedQuery("queryDono");
+		 return query.getResultList();
+	}
+	
+	public boolean remover(Dono dono) {
+		if (dono != null) {
+			
+			fabrica.getTransaction().begin();
+			Dono donoDB = fabrica.find(Dono.class, dono.getId());			
+			fabrica.remove(donoDB);
+			fabrica.getTransaction().commit();
+			fabrica.close();
+			return true;
+		}
+		
+		return false;
 	}
 }
